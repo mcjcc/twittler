256aff49301d8f140d237a43ffda3e27e149f944
Johnny Chen <jcc0821@gmail.com>
2017-07-30 11:51:23 -0700

    Add bootstrap css grid and log a message to console every time a new tweet is created

--- data_generator.js
+++ data_generator.js
@@ -51,10 +51,13 @@ for(var i = 0; i < 10; i++){
 }
 
 var scheduleNextTweet = function(){
+  console.log('Generating Random Tweet!');
   generateRandomTweet();
   setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
 scheduleNextTweet();
+
+
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
